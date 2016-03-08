a929934bc02a7a52318f9d7285730804939da3e2
Thorben Dahl <thorben@sjostrom.no>
2016-03-08 13:40:06 +0100

    Made sure DB login details aren't on Git

--- src/no/ntnu/stud/tdt4145/gruppe91/ConnectionTesting.java
+++ src/no/ntnu/stud/tdt4145/gruppe91/ConnectionTesting.java
@@ -2,16 +2,12 @@ package no.ntnu.stud.tdt4145.gruppe91;
 
 public class ConnectionTesting {
 
-	final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://mysql.stud.ntnu.no/twdahl_tdt4145prj";
-	
-	final static String USER = "twdahl_tdt4145";
-	final static String PASS = "sondrethorbenvilde";
+	// Will not work before you've created Settings.java!
+	public final static SettingsInterface SETTINGS = new Settings();
 	
 	public static void main(String[] args) {
 		try {
-			Class.forName(JDBC_DRIVER);
-			System.out.println("Det funket..?");
+			Class.forName(SETTINGS.getDriver());
 		} catch (Exception e) {
 			System.err.print(e);
 		}
---
+++ src/no/ntnu/stud/tdt4145/gruppe91/SettingsInterface.java
@@ -0,0 +1,9 @@
+package no.ntnu.stud.tdt4145.gruppe91;
+
+public interface SettingsInterface {
+	public String getPass();
+	public String getUser();
+	public String getServerAddress();
+	public String getDatabase();
+	public String getDriver();
+}
