9c73761c5beaa46e69d553c35ea18c0c72422908
Thorben Dahl <thorben@sjostrom.no>
2016-03-11 12:50:27 +0100

    Added method for getting URL

--- src/no/ntnu/stud/tdt4145/gruppe91/SettingsInterface.java
+++ src/no/ntnu/stud/tdt4145/gruppe91/SettingsInterface.java
@@ -5,5 +5,12 @@ public interface SettingsInterface {
 	public String getUser();
 	public String getServerAddress();
 	public String getDatabase();
+	/**
+	 * Input to Class.forName, that is, the package path to the Driver
+	 * @return
+	 */
 	public String getDriver();
+	default public String getConnectionURL() {
+		return "jdbc:mysql://" + this.getServerAddress() + "/" + this.getDatabase();
+	}
 }
